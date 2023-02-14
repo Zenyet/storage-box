752b5357346438ec8f225de8d11abfee335cec09
Zenyet <zenyet1998@gmail.com>
2023-02-15 00:18:10 +0800

    feat: some SEO and notFound.

---
+++ app/[...slug]/head.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// tip: still not working in development mode .
+export default function Head({ params }: { params: { slug: string & string[] } }) {
+  return (
+    // use head.js in app/?other/[dir]/head.tsx before 13.2 , yep finally i finish it, cry :(
+    <>
+      <title>{'/dev/disk0/' + params.slug.join('/')}</title>
+    </>
+  );
+}
---
+++ ui/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <>
+      <h2>Not Found</h2>
+      <p>没有该目录！</p>
+    </>
+  );
+}
