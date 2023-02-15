c0e9b32d1cbf321022d5d24aab608e31b20311ea
Zenyet <zenyet1998@gmail.com>
2023-02-15 22:13:38 +0800

    feat: the fucking https for rediSS?

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 const config = {
   OAuthURL: 'https://login.microsoftonline.com/common/oauth2/v2.0',
   graphURL: 'https://graph.microsoft.com/v1.0/me/drive/root',
-  redisURL: 'redis://default:694891fefb294811a55a5e44fb7111ff@apn1-legible-dane-34555.upstash.io:34555',
+  redisURL: 'rediss://default:694891fefb294811a55a5e44fb7111ff@apn1-legible-dane-34555.upstash.io:34555',
   redirect_url: '',
 };
 
