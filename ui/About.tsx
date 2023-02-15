20ff56ee3a76075664d81f1c262ac5d3e0063621
Zenyet <zenyet1998@gmail.com>
2023-02-15 16:16:59 +0800

    feat: beautify about page.

--- ui/About.tsx
+++ ui/About.tsx
@@ -1,10 +1,15 @@
-"use client"; // define client component
+'use client'; // define client component
 import React from 'react';
+import Background from '@/ui/Background';
 
 export default function About() {
   return (
-    <div>
-      About Page
-    </div>
-  )
+    <Background>
+      <h2 className='text-4xl font-bold py-4'>Hello! 👋</h2>
+      <p>这是我的 Onedrive 目录!</p>
+      <footer className='pt-2'>Made with ❤️ by <a
+        className='hover:bg-blue-400 px-0.5 text-white border-b-2 border-blue-400 transition-colors' target='_blank'
+        href='https://yequalsx.com'>Zenyet</a></footer>
+    </Background>
+  );
 }
