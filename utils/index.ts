4fddcd1417690f915d57b629533ae1b1625a7391
Zenyet <zenyet1998@gmail.com>
2023-02-16 16:05:31 +0800

    feat: adds error catch

--- utils/index.ts
+++ utils/index.ts
@@ -4,5 +4,7 @@ export async function request<T>(url: string, extended: RequestInit): Promise<T>
   return await (await fetch(url, {
     method: extended.method ?? 'GET',
     ...extended,
-  })).json();
+  })).json().catch(err => {
+    console.log('@Oops, request doesnt work!', err);
+  });
 }
