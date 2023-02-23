11ff53d9f0dbbca003c90db229a2485441996221
Zenyet <zenyet1998@gmail.com>
2023-02-23 16:21:24 +0800

    test: root path Link will reload.

--- ui/Navbar.tsx
+++ ui/Navbar.tsx
@@ -2,13 +2,15 @@
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
   const path = usePathname(); // i think there's no that type thing :))
+  const router = useRouter();
   return <>
     <nav
       className='w-[100%] h-[100%] backdrop-blur-md sm:backdrop-blur-none bg-mobile-bg-nav dark:bg-mobile-nav-dark  sm:bg-nav sm:dark:bg-nav-dark flex flex-row sm:flex-col justify-center items-center'>
-      <Link href='/'
+      <Link href={'/'} onClick={() => router.push('/')}
             className={'flex items-center justify-center sm:justify-start cursor-default w-1/3 sm:w-5/6 px-2 py-2 rounded-md select-none text-gray-700 dark:text-gray-200 ' + (path !== '/about' ? 'nav-active' : '')}>
         <svg className='fill-gray-600 dark:fill-zinc-400' version='1.1' xmlns='http://www.w3.org/2000/svg'
              xmlnsXlink='http://www.w3.org/1999/xlink'
